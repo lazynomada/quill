a5cc9b86624481e46b330ee920c288c947348f88
Bryan Astacio <lazynomada@gmail.com>
2025-03-01 18:50:12 -0400

    feat: clean the application to start working on features

--- extension/src/entrypoints/content.ts
+++ extension/src/entrypoints/content.ts
@@ -1,6 +1,33 @@
+import { mount } from "svelte"
+import { ContentScriptContext, createShadowRootUi } from "wxt/client"
+
+import Counter from "@/components/Counter.svelte"
+
+import './style.css'
+
+async function mountSvelte(container: HTMLElement) {
+  mount(Counter, { target: container })
+}
+
+async function mountUI(ctx: ContentScriptContext) {
+  const element = document.querySelectorAll('center')
+  const anchor = element[element.length - 1]  
+  
+  return await createShadowRootUi(ctx, {
+    anchor,
+    name: 'ui-quill',
+    position: 'inline',
+    css: 'display: inline-block',
+    onMount: mountSvelte
+  })
+}
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ['<all_urls>'],
+  cssInjectionMode: 'ui',
+  main(ctx) {
+
+    mountUI(ctx).then(c => c.mount())
+    console.log('Hello content.')
   },
-});
+})
