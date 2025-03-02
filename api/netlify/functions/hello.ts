4cd70dbc39d5be05c05a33959941af7dea8fbe35
Bryan Astacio <lazynomada@gmail.com>
2025-03-02 16:20:46 -0400

    chore: testing ts paths with netlify

--- api/netlify/functions/hello.ts
+++ api/netlify/functions/hello.ts
@@ -1,5 +1,6 @@
 import type { Context } from "@netlify/functions";
+import HelloWorld from "@/HelloWorld";
 
 export default async (req: Request, context: Context) => {
-  return new Response("Hello, world!")
+  return new Response(HelloWorld())
 }
